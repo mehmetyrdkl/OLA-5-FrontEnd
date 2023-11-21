ffa492cdbb1da266457ab4ee0bcf334d21d5f5ea
Mehmet <mehmet.h.yurdakul@gmail.com>
2023-11-21 14:37:30 +0100

    small changes
    Co-authored-by: Thordur Frimann <ThordurTh@users.noreply.github.com>
    Co-authored-by: Martin Windsor <windsorm@users.noreply.github.com>

--- app/components/Cards.js
+++ app/components/Cards.js
@@ -1,58 +1,67 @@
 import React from "react";
 import "../styles/cards.scss";
+import Image from "next/image";
 
 const Cards = () => {
   return (
     <div className="cards-container">
       <div className="card">
-        <img
+        <Image
           src="https://cdn.dwarf.dk/comwell-cms-production/img/containers/main/kampagner/b2b_efter%C3%A5r2023/b2b_kampagnefoto.jpg/2d07f5cd2afd708064ef21f9677cf289.webp"
           alt="Image 1"
           className="card-image"
+          layout="fill"
+          objectFit="cover"
         />
         <div className="card-tag">
-          <p>See premises and meeting packages</p>
+          <p>See venues and meeting packages</p>
         </div>
         <div className="card-text">
           <p className="card-text-title">
             Let us help you with your next meeting
           </p>
           <p className="card-text-details">
-            We have the primeses, the good catering and professional meeting
-            planing.
+            We offer meeting space, catering, and professional meeting planning.
           </p>
         </div>
       </div>
       <div className="card">
-        <img
+        <Image
           src="https://cdn.dwarf.dk/comwell-cms-production/img/containers/main/jul/jul-paa-hotellerne/comwell-centralvaerkstedet-julestemning.jpg/bfb70417588c1835e0dd5823f6adc91c.webp"
           alt="Image 2"
           className="card-image"
+          layout="fill"
+          objectFit="cover"
         />
         <div className="card-tag">
-          <p>Put a mark in your calendar</p>
+          <p>Mark your calendar</p>
         </div>
         <div className="card-text">
           <p className="card-text-title">Christmas party with food and music</p>
           <p className="card-text-details">
-            Ready for this year's company Christmas lunch? See the hotels'
-            Chrimstas offers for you.
+            Ready for your company&apos;s Christmas party? Check out the
+            hotels&apos; Chrimstas deals.
           </p>
         </div>
       </div>
       <div className="card">
-        <img
+        <Image
           src="https://cdn.dwarf.dk/comwell-cms-production/img/containers/main/kampagner/black_weekend_2023/spil2_bw.jpg/925fa776956f562d5e94a6a80b27fac8.webp"
           alt="Image 3"
           className="card-image"
+          layout="fill"
+          objectFit="cover"
         />
         <div className="card-tag">
-          <p>Play and win</p>
+          <p>Wind a dream getaway</p>
         </div>
         <div className="card-text">
-          <p className="card-text-title">Black Weekend with offers</p>
+          <p className="card-text-title">
+            Get ready for Black Weekend with our exclusive deals
+          </p>
           <p className="card-text-details">
-            Try your luck in our on-armed thief as we warm up to black Weekend.
+            Spin out one-armed bandit for a chance to win big before Black
+            Weekend
           </p>
         </div>
       </div>
